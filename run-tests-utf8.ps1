e62177a0cc166af5cc176f052ba7f9b34249be5f
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-01-03 23:34:17 +0700

    refactor: Refactor code structure for improved readability and maintainability

--- run-tests-utf8.ps1
+++
@@ -1,9 +0,0 @@
-# PowerShell script to run tests with UTF-8 encoding
-# Usage: .\run-tests-utf8.ps1
-
-# Set console to UTF-8
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$env:JAVA_TOOL_OPTIONS = "-Dfile.encoding=UTF-8"
-
-# Run Maven tests
-mvn clean test -Dbrowser=chrome -Dremote=true -Dheadless=true
